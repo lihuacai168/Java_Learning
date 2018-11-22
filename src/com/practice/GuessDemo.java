f2a630b96b5ce24bc625f1d98a008992749672d0
Rikasai_home <lihuacai168@gmail.com>
2018-11-22 11:45:30 +0800

    update guessed content

--- src/com/practice/GuessDemo.java
+++ src/com/practice/GuessDemo.java
@@ -13,23 +13,46 @@ public class GuessDemo {
         //%9,取模变成[0,8]的double
         //+1,然后(int),,变成int类型,范围是[0,9]
         final int guess_num = (int) (((Math.random() * 100)%9)+1)*1000;//用户要猜的数字
-
+        System.out.println(guess_num);
         //用户猜的次数
 
         int count = 0;
         int use_num = -1;
+        String bonusName = null;
         for (int i = 0; i < 5; i++) {
-
             System.out.println("请输入神秘数字: "+ ++count +"次");
             use_num = new Scanner(System.in).nextInt();
             if (use_num == guess_num ){
-                System.out.println("中奖了");
+                switch (count){
+                    case 1:
+                        bonusName = "一等奖";
+                        break;
+                    case 2:
+                        bonusName = "二等奖";
+                        break;
+                    case 3:
+                        bonusName = "三等奖";
+                        break;
+                    default:
+                        bonusName = "安慰奖";
+                }
                 break;
             }
             else {
                 System.out.println("错了哦,请继续");
             }
+        }
+
+
+        if(bonusName == null){
+            System.out.println("很遗憾,5次都没中奖");
+        }
+        else {
+            System.out.printf("恭喜你中了%s\n",bonusName);
 
         }
+
+
+
     }
 }
